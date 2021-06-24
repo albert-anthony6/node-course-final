2f53c88d24ec63434432464cc7b87b337897691e
Albert Valdes <albert.anthony1999@gmail.com>
2021-06-24 02:12:50 -0500

    Installed validator package

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -42,7 +43,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'Tour must have a price.'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: 'Discount price should be below the regular price.',
+      },
+    },
     summary: {
       type: String,
       trim: true,
