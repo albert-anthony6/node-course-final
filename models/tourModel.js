27c5d189fbada39fe77ae1c68900e6ce7c7da3c0
Albert Valdes <albert.anthony1999@gmail.com>
2021-06-24 01:46:37 -0500

    Added validators to Schema and aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'Tour must have a name.'],
       trim: true,
       unique: true,
+      maxLength: [40, 'A tour name must have less or equal than 40 chars.'],
+      minLength: [10, 'A tour name must have more or equal than 10 chars.'],
     },
     slug: String,
     duration: {
@@ -21,10 +23,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty.'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, or difficult.',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -94,7 +102,12 @@ tourSchema.pre(/^find/, function (next) {
 // Post query Middleware
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
-  console.log(docs);
+  next();
+});
+
+// Aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   next();
 });
 
