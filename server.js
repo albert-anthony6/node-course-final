74b48895f7391d3b0a3fcb919e324f496e1c3da3
Albert Valdes <albert.anthony1999@gmail.com>
2021-06-29 23:09:29 -0500

    Global handler for uncaught promise exceptions

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+// Global handler for uncaught promise exceptions (uncaught sync functions)
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED EXCEPTION! Shutting down...');
+  process.exit(1);
+});
+
 const app = require('./app');
 
 mongoose
@@ -18,7 +25,7 @@ const server = app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });
 
-// Global handler for unhandled promise rejections
+// Global handler for unhandled promise rejections (unhandled async functions)
 process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION! Shutting down...');
